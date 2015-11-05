f72372f332e62964e0f0d9f73347747408dd578f
Viachaslau Tyshkavets <pmstss@gmail.com>
2015-11-05 19:04:19 +0300

    - usage.js added as usage sample - proper error handling - RSVP replaced with es6 Promise - some es6 features utilized (arrow functions, - EpubCfiGenerator now can be constructed w/o "new"

--- epub-cfi-generator.js
+++ epub-cfi-generator.js
@@ -1,7 +1,6 @@
 var fs = require('fs');
 var path = require('path');
 var xmldom = require('xmldom');
-var RSVP = require('rsvp');
 var AdmZip = require('adm-zip');
 var tmp = require('tmp');
 
@@ -22,16 +21,19 @@ var cfiGenerator = require('./readium-cfi/cfi_generator').Generator;
     }
 
     function getPackageFilePath(epubPath) {
-        return new RSVP.Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             var filePath = path.join(epubPath, 'META-INF/container.xml');
             fs.readFile(filePath, 'utf8', function (err, data) {
-                if (err) {
-                    reject('Error reading package file: ' + filePath);
-                    return;
+                try {
+                    if (err) {
+                        throw `Error reading container.xml file ${packageFilePath}: ${err}`;
+                    }
+                
+                    resolve(path.join(epubPath, validateSingleNode(xpathUtils.nsXPath(xpathUtils.NS_CONTAINER, '//main:rootfile',
+                        domParser.parseFromString(data))).getAttribute('full-path')));
+                } catch (e) {
+                    reject(e);
                 }
-
-                resolve(validateSingleNode(xpathUtils.nsXPath(xpathUtils.NS_CONTAINER, '//main:rootfile',
-                    domParser.parseFromString(data))).getAttribute('full-path'));
             });
         });
     }
@@ -39,108 +41,99 @@ var cfiGenerator = require('./readium-cfi/cfi_generator').Generator;
     function getSpinesInfo(packageFilePath) {
         var rootDir = path.dirname(packageFilePath);
 
-        return new RSVP.Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             fs.readFile(packageFilePath, 'utf8', function (err, data) {
-                if (err) {
-                    reject('Error reading package file: ' + packageFilePath);
-                    return;
+                try {
+                    if (err) {
+                        throw `Error reading package file ${packageFilePath}: ${err}`;
+                    }
+
+                    var doc = domParser.parseFromString(data);
+                    var itemrefs = xpathUtils.nsXPath(xpathUtils.NS_PACKAGE_DOC, '//main:spine//main:itemref', doc);
+                    //console.log(`spines found: ${itemrefs.length}`);
+
+                    resolve(itemrefs.map(function (itemref) {
+                        var item = validateSingleNode(xpathUtils.nsXPath(xpathUtils.NS_PACKAGE_DOC,
+                                '//main:item[@id="' + itemref.getAttribute('idref') + '"]', doc));
+
+                        return {
+                            idref: itemref.getAttribute('idref'),
+                            href: item.getAttribute('href'),
+                            path: rootDir + '/' + item.getAttribute('href')
+                        };
+                    }));
+                } catch (e) {
+                    reject(e);
                 }
-
-                var doc = domParser.parseFromString(data);
-                var itemrefs = xpathUtils.nsXPath(xpathUtils.NS_PACKAGE_DOC, '//main:spine//main:itemref', doc);
-                console.log('spines found: %d', itemrefs.length);
-
-                resolve(itemrefs.map(function (itemref) {
-                    var item = validateSingleNode(xpathUtils.nsXPath(xpathUtils.NS_PACKAGE_DOC,
-                            '//main:item[@id="' + itemref.getAttribute('idref') + '"]', doc));
-
-                    return {
-                        idref: itemref.getAttribute('idref'),
-                        href: item.getAttribute('href'),
-                        path: rootDir + '/' + item.getAttribute('href')
-                    };
-                }));
             });
         });
     }
 
     function getSpineNodesCfi(spineFilePath) {
-        return new RSVP.Promise(function (resolve, reject) {
+        return new Promise(function (resolve, reject) {
             fs.readFile(spineFilePath, 'utf8', function (err, data) {
-                if (err) {
-                    reject('Error reading spine file: ' + spineFilePath);
-                    return;
+                try {
+                    if (err) {
+                        throw `Error reading spine file ${spineFilePath}: ${err}`;
+                    }
+
+                    var doc = domParser.parseFromString(data);
+
+                    var textNodes = xpathUtils.htmlXPath('/main:html/main:body//text()[normalize-space()]', doc);
+                    console.log('spine file %s, length: %d, number of text nodes: %d', spineFilePath, data.length,
+                        textNodes.length);
+
+                    resolve(textNodes.map(function (node) {
+                        var cfi = cfiGenerator.generateCharacterOffsetCFIComponent(node, null, null, null);
+                        return {
+                            node: node.toString(),
+                            cfi: cfi
+                        };
+                    }));
+                } catch (e) {
+                    reject(e);
                 }
-
-                var doc = domParser.parseFromString(data);
-
-                var textNodes = xpathUtils.htmlXPath('/main:html/main:body//text()[normalize-space()]', doc);
-                console.log('spine file %s, length: %d, number of text nodes: %d', spineFilePath, data.length,
-                    textNodes.length);
-
-                resolve(textNodes.map(function (node) {
-                    var cfi = cfiGenerator.generateCharacterOffsetCFIComponent(node, null, null, null);
-                    return {
-                        node: node.toString(),
-                        cfi: cfi
-                    };
-                }));
             });
         });
     }
 
     function EpubCfiGenerator() {
-
+        if (!(this instanceof EpubCfiGenerator)) {
+            return new EpubCfiGenerator();
+        }
     }
 
-    //TODO check errors handling
-    EpubCfiGenerator.prototype.parse = function (inputPath, writeToFile) {
-        var pathInfo = (function () {
-            var isDirectory = fs.lstatSync(inputPath).isDirectory();
-
-            var zipAwareInputPath;
-            if (!isDirectory) {
+    EpubCfiGenerator.prototype.parse = function (inputPath, writeToFile) {         
+        return new Promise(function (resolve, reject) {
+            fs.lstat(inputPath, function (err, stats) {
+                if (!err) {
+                    resolve(stats);
+                } else {
+                    reject(err);
+                }
+            });
+        }).then(function (stats) {       
+            if (!stats.isDirectory()) {
                 console.time('zipExtract');
                 tmp.setGracefulCleanup();
                 var tmpDirObj = tmp.dirSync({});
                 var zip = new AdmZip(inputPath);
                 zip.extractAllTo(tmpDirObj.name.toString(), true);
                 console.timeEnd('zipExtract');
-
-                zipAwareInputPath = tmpDirObj.name;
+                
+                return tmpDirObj.name;
             } else {
-                zipAwareInputPath = inputPath;
+                return inputPath;
             }
-
-            var outDirPath = isDirectory ? path.join(inputPath, '..') : path.dirname(inputPath);
-            var outFilePath = path.join(outDirPath, path.basename(inputPath) + '.json');
-            return {
-                inputPath: zipAwareInputPath,
-                outFilePath: outFilePath
-            };
-        })();
-        console.log('pathInfo: %s', JSON.stringify(pathInfo));
-
-        return new RSVP.Promise(function (resolve, reject) {
-            getPackageFilePath(pathInfo.inputPath).then(function (packageFilePath) {
-                return getSpinesInfo(path.join(pathInfo.inputPath, packageFilePath));
-            }, reject).then(function (spinesInfo) {
-                return RSVP.all(spinesInfo.map(function (spineInfo) {
-                    return getSpineNodesCfi(spineInfo.path).then(function (nodes) {
-                        spineInfo.content = nodes;
-                        delete spineInfo.path;
-                    }, reject);
-                })).then(function () {
-                    return spinesInfo;
-                }, reject);
-            }, reject).then(function (spinesInfo) {
-                if (writeToFile) {
-                    var serialized = JSON.stringify(spinesInfo, null, 4);
-                    console.log('result data length: %d', serialized.length);
-                    fs.writeFileSync(pathInfo.outFilePath, serialized);
-                }
-                resolve(spinesInfo);
-            }, reject);
+        }).then(getPackageFilePath).then(function (packageFilePath) {
+            return getSpinesInfo(packageFilePath);
+        }).then(function (spinesInfo) {
+            return Promise.all(spinesInfo.map(function (spineInfo) {
+                return getSpineNodesCfi(spineInfo.path).then(function (nodes) {
+                    spineInfo.content = nodes;
+                    delete spineInfo.path;
+                });
+            }));
         });
     };
 
---
+++ usage.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var EpubCfiGenerator = require('./epub-cfi-generator');
+
+(function () {
+    'use strict';
+    
+    var inputFile = process.argv[2];
+    var outputFile = process.argv[3];
+    if (!inputFile || !outputFile) {
+        console.log('usage: node usage <input_epub_file> <output_json_file>');
+        return;
+    }
+    
+    EpubCfiGenerator().parse(inputFile).then(function (spinesInfo) {
+        var serialized = JSON.stringify(spinesInfo, null, 4);
+        console.log(`result data length: ${serialized.length}`);
+        fs.writeFileSync(outputFile, serialized);
+    }).catch(err => console.error(err + '\x07'));
+})();
